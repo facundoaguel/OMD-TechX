4d2cd57e1ec49cf3d09bce267f033285a9ed7dcd
Facundo Aguel <facundoaguel@gmail.com>
2023-05-29 14:35:38 -0300

    Turnos, cambio modelo y migracion

---
+++ OMD-TechX/Migrations/20230529173430_manu.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OMD_TechX.Migrations
+{
+    /// <inheritdoc />
+    public partial class manu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Fecha",
+                table: "Turnos",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Perro",
+                table: "Turnos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "estado",
+                table: "Turnos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "motivo",
+                table: "Turnos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "motivoId",
+                table: "Turnos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "usuarioId",
+                table: "Turnos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Perro",
+                table: "Turnos");
+
+            migrationBuilder.DropColumn(
+                name: "estado",
+                table: "Turnos");
+
+            migrationBuilder.DropColumn(
+                name: "motivo",
+                table: "Turnos");
+
+            migrationBuilder.DropColumn(
+                name: "motivoId",
+                table: "Turnos");
+
+            migrationBuilder.DropColumn(
+                name: "usuarioId",
+                table: "Turnos");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Fecha",
+                table: "Turnos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
--- OMD-TechX/Modelos/Turno.cs
+++ OMD-TechX/Modelos/Turno.cs
@@ -1,4 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using static System.Net.WebRequestMethods;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OMD_TechX.Modelos
 {
@@ -6,8 +10,71 @@ namespace OMD_TechX.Modelos
     {
         [Key]
         public int Id { get; set; }
-        public DateTime Fecha { get; set; }
+        public DateTime? Fecha { get; set; }
         public string Franja { get; set; }
         public int PerroId { get; set; }
+        public string Perro { get; set; }
+        public string estado { get; set; }
+        public int motivoId { get; set; }
+        public string motivo { get; set; }
+        public int usuarioId { get; set; }
+
+        public Turno() { }
+
+        public static async Task<string> ObtenerPerro(int Id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://localhost:7083/api/perros/{Id}");
+                    response.EnsureSuccessStatusCode(); // Verificar que la respuesta sea exitosa
+                    var perro = await response.Content.ReadFromJsonAsync<Perro>();
+                    return perro.Nombre.ToString();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"No hay perros cargados: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        public static async Task<string> ObtenerMotivo(int Id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://localhost:7083/api/atenciones/{Id}");
+                    response.EnsureSuccessStatusCode(); // Verificar que la respuesta sea exitosa
+                    var atencion = await response.Content.ReadFromJsonAsync<Atencion>();
+                    return atencion.Nombre.ToString();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"No hay atenciones cargadas: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        public async Task EstablecerEstadoPorIdAsync(int perroId, int motivoId)
+        {
+            Perro = await ObtenerPerro(perroId);
+            motivo = await ObtenerMotivo(motivoId);
+        }
+
+        public Turno(DateTime fecha, String franja, int perroId, int motivoId, int usuarioId)
+        {
+            this.usuarioId = usuarioId;
+            this.Fecha = fecha;
+            this.Franja = franja;
+            this.estado = "Pendiente";
+            this.PerroId = perroId;
+            this.motivoId = motivoId;
+            Task.Run(async () => await EstablecerEstadoPorIdAsync(perroId, motivoId)).Wait();
+        }
     }
+
 }
