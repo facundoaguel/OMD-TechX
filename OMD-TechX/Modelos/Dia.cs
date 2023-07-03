e3082b8e53969e23eee6caebf75344ba6257f968
Juana Borrelli Zara <jborrellizara@gmail.com>
2023-07-03 17:03:06 -0300

    calendario hecho en el front

---
+++ OMD-TechX/Modelos/Dia.cs
@@ -0,0 +1,14 @@
+﻿namespace OMD_TechX.Modelos
+{
+    public class Dia
+    {
+        public int Id { get; set; }
+        public DateTime FechaEvent { get; set; } = new DateTime(1900, 1, 1);
+        public DateTime FechaDesde { get; set; } = new DateTime(1900, 1, 1);
+        public DateTime FechaHasta { get; set; } = new DateTime(1900, 1, 1);
+        public string FechaValue { get; set; }
+        public string DiaName { get; set;}
+
+        public string VetDeTurno { get; set; }
+    }
+}
